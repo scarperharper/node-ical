0c32894867bbe3d3205a32f8c51d8212185e3c99
Simon Harper <simon@wustl.uk>
2022-01-17 18:13:02 +0000

    Support loading as ESM

--- ical.js
+++ ical.js
@@ -103,8 +103,7 @@ const addTZ = function (dt, parameters) {
 let zoneTable = null;
 function getIanaTZFromMS(msTZName) {
   if (!zoneTable) {
-    const p = require('path');
-    zoneTable = require(p.join(__dirname, 'windowsZones.json'));
+	zoneTable = readFileSync(new URL('./windowsZones.json', import.meta.url));
   }
 
   // Get hash entry
